ffe8ee44aa727de30bc2b786e8c7207be5239e6f
Jason Jacobs <jayj3.0pro@gmail.com>
2023-11-24 13:30:47 -0700

    calc update

--- Projects-Gallery/Calculator/calculator.py
+++ Projects-Gallery/Calculator/calculator.py
@@ -6,5 +6,11 @@ from tkinter import *
 root = Tk()
 root.geometry('600x400')
 root.title("Calculator")
-result = 
+result = Entry(root, width=500,)
+result.place(x=100, y=200)
+message = Label(root,text="Math is best when done at a desk!", font='arial 15 bold')
+message.place(x=100, y=10)
+
+#but1 = Button(root,text= 'Name', font= 'arial 12', bg='#5CA', command = genInfo).place(x=80, y=300)
+
 root.mainloop()
--- Projects-Gallery/Stock-app/StockTool.py
+++ Projects-Gallery/Stock-app/StockTool.py
@@ -2,7 +2,7 @@ import yfinance as yf
 from tkinter import *
 
 
-def share():
+def ownership():
     global message
     stk = stock.get()
     stk = stk.upper()
@@ -61,7 +61,7 @@ message.place(x=100, y=100)
 
 but1 = Button(root,text= 'Name', font= 'arial 12', bg='#5CA', command = genInfo).place(x=80, y=300)
 but2 = Button(root,text= 'Dividend', font= 'arial 12', bg='#5AC', command = div).place(x=170, y=300)
-but3 = Button(root,text= 'Ownership', font= 'arial 12', bg='#A5C', command = share).place(x=280, y=300)
+but3 = Button(root,text= 'Ownership', font= 'arial 12', bg='#A5C', command = ownership).place(x=280, y=300)
 but4 = Button(root,text= 'Holders', font= 'arial 12', bg='#CA5', command = holdr).place(x=400, y=300)
 clr = Button(root, text= 'Clear', font= 'arial 12', bg='#5FF', command= clearit).place(x=300, y=350)
 
